827d3ad6ad060502b722deb612de06e9b56ace5a
khml <43922054+khml@users.noreply.github.com>
2020-10-03 20:26:06 +0900

    [New] init HTTP Server

---
+++ yaml-mock-server/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	println("Running Server now")
+
+	http.HandleFunc("/", helloWorld)
+
+	_ = http.ListenAndServe(":3000", nil)
+}
+
+func loggingRequest(r *http.Request) {
+	log.Printf("[%s] %s FROM %s\n", r.Method, r.URL.Path, r.RemoteAddr)
+}
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	loggingRequest(r)
+
+	_, err := w.Write([]byte("Hello World"))
+	if err != nil {
+		println(err)
+	}
+}
