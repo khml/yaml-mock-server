1b6677a527eeefa7ac532adb5d2becb3710e05a9
khml <43922054+khml@users.noreply.github.com>
2020-10-03 20:30:51 +0900

    [Add] read YAML

--- yaml-mock-server/main.go
+++ yaml-mock-server/main.go
@@ -1,11 +1,50 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+
+	"gopkg.in/yaml.v2"
 )
 
 func main() {
+	readRouting()
+}
+
+type Route struct {
+	Path string
+	File string
+}
+
+func readRouting() {
+	buf, err := ioutil.ReadFile("./sample.yml")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	routes, err := readRouteingFromYaml(buf)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(routes)
+}
+
+func readRouteingFromYaml(fileBuffer []byte) ([]Route, error) {
+	defaultSize := 10
+	routes := make([]Route, defaultSize)
+	err := yaml.Unmarshal(fileBuffer, &routes)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	return routes, nil
+}
+
+func routing() {
 	println("Running Server now")
 
 	http.HandleFunc("/", helloWorld)
